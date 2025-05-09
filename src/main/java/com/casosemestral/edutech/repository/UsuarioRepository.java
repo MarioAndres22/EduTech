f58db02e7ec14ea77e4f986c750c8be8dd086525
Melipilla <melipilla@duoc.acad>
2025-05-09 14:42:10 -0400

    correccion repository

--- src/main/java/com/casosemestral/edutech/repository/UsuarioRepository.java
+++ src/main/java/com/casosemestral/edutech/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.casosemestral.edutech.repository;
 
 import com.casosemestral.edutech.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,11 +10,9 @@ import java.util.List;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    List<Usuario> findByApellido(String Apellido);
 
-    List<Usuario> findByCorreo(String Correo);
 
-    List<Usuario> findByNombreAndApellido(String Nombre , String Apellido);
+
 
 
 
