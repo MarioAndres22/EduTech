97c0827669740461f29331044b063fd355aadf54
Melipilla <melipilla@duoc.acad>
2025-05-09 14:43:31 -0400

    UsuarioService

---
+++ src/main/java/com/casosemestral/edutech/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package com.casosemestral.edutech.service;
+
+import com.casosemestral.edutech.model.Usuario;
+import com.casosemestral.edutech.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(long id) {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
