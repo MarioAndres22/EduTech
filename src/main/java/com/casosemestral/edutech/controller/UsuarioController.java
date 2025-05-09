763d465e7fd98cafa17beb6ba239ba0644040ba7
Melipilla <melipilla@duoc.acad>
2025-05-09 15:38:30 -0400

    controller actualizacion

--- src/main/java/com/casosemestral/edutech/controller/UsuarioController.java
+++ src/main/java/com/casosemestral/edutech/controller/UsuarioController.java
@@ -3,10 +3,9 @@ package com.casosemestral.edutech.controller;
 import com.casosemestral.edutech.model.Usuario;
 import com.casosemestral.edutech.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +25,47 @@ public class UsuarioController {
         }
         return ResponseEntity.ok(usuarios);
     }
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardar(@RequestBody Usuario usuario){
+        Usuario usuarioNuevo= usuarioService.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioNuevo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscar(@PathVariable Integer id){
+        try{
+            Usuario usuario = usuarioService.findById(id);
+            return ResponseEntity.ok(usuario);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizar(@PathVariable Integer id,@RequestBody Usuario usuario){
+        try{
+            Usuario user= UsuarioService.findById(id);
+            user.setId(id);
+            user.setRun(usuario.getRun());
+            user.setNombre(usuario.getNombre());
+            user.setApellido(usuario.getApellido());
+            user.setCorreo(usuario.getCorreo());
+
+            usuarioService.save(user);
+            return ResponseEntity.ok(usuario);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        try{
+            usuarioService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
