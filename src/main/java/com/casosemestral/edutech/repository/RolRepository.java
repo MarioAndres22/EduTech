1b1be165149f55284909bf6c3b225821af393dec
Benjamin Bizama <bizamabenjamin56@gmail.com>
2025-05-22 14:07:40 -0400

    arreglado

--- src/main/java/com/casosemestral/edutech/repository/RolRepository.java
+++ src/main/java/com/casosemestral/edutech/repository/RolRepository.java
@@ -2,9 +2,9 @@ package com.casosemestral.edutech.repository;
 
 import com.casosemestral.edutech.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(String nombre);
 }
-
--- src/main/java/com/casosemestral/edutech/service/RolService.java
+++ src/main/java/com/casosemestral/edutech/service/RolService.java
@@ -4,9 +4,6 @@ import com.casosemestral.edutech.model.Rol;
 import com.casosemestral.edutech.repository.RolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
 
 import java.util.List;
 import java.util.Optional;
@@ -28,9 +25,4 @@ public class RolService {
     public Optional<Rol> obtenerPorId(Long id) {
         return rolRepository.findById(id);
     }
-
-    public interface RolRepository extends JpaRepository<Rol, Long> {
-        Optional<Rol> findByNombre(String nombre);
-    }
-
 }
